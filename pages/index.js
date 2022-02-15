7d8c46d499a91ad523a301ffddf64dea91f93e6a
unknown <raphael.d.caetano48@gmail.com>
2022-02-15 11:51:30 -0300

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,8 @@ export default function Home() {
       </Head>
       <Nav />
       <Profile />
-
       <main className={styles.main}>
-
+      
         <Cadastro />
         <hr />
         <Incluir />
