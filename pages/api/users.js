23f4ba6dadc4bdfa88b1ae04f0fed42fe95c2bad
unknown <raphael.d.caetano48@gmail.com>
2022-02-18 13:08:03 -0300

    Alteração no sistema de API

---
+++ pages/api/users.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+        res.status(200).json([{ name: 'Produto 1' }, { name: 'Produto 2' }])
+    } else if (req.method === 'PUT') {
+        res.status(200).json('Mensagem nova')
+    }
+    else if (req.method === 'PATCH') {
+        res.status(200).json('produto adicionado com sucesso')
+    }
+    else if (req.method === 'DELETE') {
+        res.status(200).json('produto Deletado com sucesso')
+    }
+    else {
+        res.status(405).end('Method not allowed')
+    }
+}
